67130de6a7b115029af4e38acc71bf5b3459db9e
OliveTheLemon <olive.the.lemon@gmail.com>
2026-07-28 16:03:00 +0900

    [C4] 우측개별메뉴(종합배경), 좌우슬라이드, 모달팝업, 190px, 270px

---
+++ C4/script/index.js
@@ -0,0 +1,46 @@
+$(function () {
+  // ----- 메뉴
+  $("#menu > li").mouseover(function () {
+    $(this).children("a").addClass("m-hover");
+    $(this).find(".submenu").stop().slideDown();
+    $("#menu-bg").stop().slideDown();
+  });
+  $("#menu > li").mouseout(function () {
+    $(this).children("a").removeClass("m-hover");
+    $(this).find(".submenu").stop().slideUp();
+    $("#menu-bg").stop().slideUp();
+  });
+
+  // ----- 슬라이드
+  $("#slide-wrap").append($(".slide").first().clone(true));
+
+  let currentIdx = 0;
+  const imgCnt = 3;
+
+  setInterval(function () {
+    currentIdx++;
+
+    $("#slide-wrap").animate({ marginLeft: -currentIdx * 100 + "%" }, 600);
+
+    if (currentIdx === imgCnt) {
+      setTimeout(function () {
+        $("#slide-wrap").animate({ marginLeft: 0 }, 0);
+        currentIdx = 0;
+      }, 700);
+    }
+  }, 3000);
+
+  // ----- 팝업
+  $("#notice .c-body li:first-child").click(function (e) {
+    e.preventDefault();
+
+    $("#modal-bg").show();
+    $("#popup").show();
+  });
+  $("#close-btn > a").click(function (e) {
+    e.preventDefault();
+
+    $("#modal-bg").hide();
+    $("#popup").hide();
+  });
+});
