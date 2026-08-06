e0284c2e52651ca088e49db88f937838b454acf4
OliveTheLemon <olive.the.lemon@gmail.com>
2026-08-06 12:20:45 +0900

    [E1] 하단개별메뉴, 좌우슬라이드, 합업, 배너, 바로가기

---
+++ E1/script/index.js
@@ -0,0 +1,39 @@
+$(function () {
+  // ----- 메뉴
+  $("#menu > li").mouseover(function () {
+    $(this).children("a").addClass("m-hover");
+    $(this).find(".submenu").stop().slideDown();
+  });
+  $("#menu > li").mouseout(function () {
+    $(this).children("a").removeClass("m-hover");
+    $(this).find(".submenu").stop().slideUp();
+  });
+  // ----- 슬라이드
+  $("#slide-wrap").append($(".slide").first().clone(true));
+
+  let currentIdx = 0;
+  const imgCnt = 3;
+  setInterval(function () {
+    currentIdx++;
+
+    $("#slide-wrap").animate({ marginLeft: -currentIdx * 100 + "%" }, 600);
+
+    if (currentIdx === imgCnt) {
+      setTimeout(function () {
+        $("#slide-wrap").animate({ marginLeft: 0 }, 0);
+        currentIdx = 0;
+      }, 700);
+    }
+  }, 3000);
+  // ----- 팝업
+  $("#notice .t-body li:first-child").click(function (e) {
+    e.preventDefault();
+    $("#modal-bg").show();
+    $("#popup").show();
+  });
+  $("#close-btn > a").click(function (e) {
+    e.preventDefault();
+    $("#modal-bg").hide();
+    $("#popup").hide();
+  });
+});
