b62506c9f787ad9820193eddf43b906fe1508262
OliveTheLemon <olive.the.lemon@gmail.com>
2026-09-03 15:19:53 +0900

    [F4] 하단전체배경메뉴, 상하슬라이드, 모달팝업

---
+++ F4-1/script/index.js
@@ -0,0 +1,48 @@
+$(function () {
+  // ------ 메뉴
+  $("#menu > li").mouseover(function () {
+    $(this).children("a").addClass("m-hover");
+    $(".submenu").stop().slideDown();
+    $(".menu-bg").stop().slideDown();
+  });
+  $("#menu > li").mouseout(function () {
+    $(this).children("a").removeClass("m-hover");
+    $(".submenu").stop().slideUp();
+    $(".menu-bg").stop().slideUp();
+  });
+  // ------ 슬라이드
+  $("#slide-wrap").append($(".slide").first().clone(true));
+
+  const imgCnt = 3;
+  const slideHeight = 350;
+  let currentIdx = 0;
+
+  setInterval(function () {
+    currentIdx++;
+
+    $("#slide-wrap").animate(
+      { marginTop: -currentIdx * slideHeight + "px" },
+      600,
+    );
+
+    if (currentIdx === imgCnt) {
+      setTimeout(function () {
+        $("#slide-wrap").animate({ marginTop: 0 }, 0);
+        currentIdx = 0;
+      }, 700);
+    }
+  }, 3000);
+  // ------ 팝업
+  $("#notice .t-body li:first-child").click(function (e) {
+    e.preventDefault();
+
+    $(".modal-bg").show();
+    $("#popup").show();
+  });
+  $("#close-btn >a").click(function (e) {
+    e.preventDefault();
+
+    $(".modal-bg").hide();
+    $("#popup").hide();
+  });
+});
